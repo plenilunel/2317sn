6105e6091bd0de464f347b9a51ec2aa29c4fd2cd
plenilunel <mugec215@gmail.com>
2023-06-17 21:02:53 +0900

    Implement Mission(1)

--- src/ScoreBoard.hpp
+++ src/ScoreBoard.hpp
@@ -21,7 +21,11 @@ private:
     int poison_score{};
     int total_score{};
 
-    long time;
+    char stage1_mission1{'X'};
+    char stage1_mission2{'X'};
+    char stage1_mission3{'X'};
+
+    long time{};
     chrono::time_point<chrono::steady_clock> start_time{};
     chrono::time_point<chrono::steady_clock> curr_time{};
     void updateScoreWin();
@@ -54,7 +58,7 @@ void ScoreBoard::awake(int h, int w, int starty, int startx) {
     start_x = startx;
 
     score_win = newwin(height/2 + height/3, width, start_y, start_x);
-    mission_win = newwin(height/2 + height/4, width, start_y*3, start_x);
+    mission_win = newwin(height/2 + height/4, width, start_y +height/2 + height/3, start_x);
 
     wbkgd(score_win, COLOR_PAIR(10) | A_BOLD);
     wbkgd(mission_win, COLOR_PAIR(10) | A_BOLD);
@@ -103,7 +107,6 @@ void ScoreBoard::calcTotal_Score() {
             gate_score*200 -
             poison_score*100 +
             (int)time;
-
 }
 
 void ScoreBoard::setSnakePos(int x, int y) {
@@ -132,9 +135,16 @@ void ScoreBoard::updateTime() {
     time = chrono::duration_cast<chrono::seconds>(curr_time - start_time).count();
 }
 
-
 void ScoreBoard::updateMissionWin() {
     wborder(mission_win, '|', '|', '-', '-', '+', '+', '+', '+');
+
+    wattron(mission_win, A_DIM);
+    mvwprintw(mission_win, 1, 3, "<Mission list>");
+    mvwprintw(mission_win, 2, 3, "1. Obtain 3 or more Growth item         ( %c )", stage1_mission1);
+    mvwprintw(mission_win, 3, 3, "2. Alive 60 seconds                     ( %c )", stage1_mission2);
+    mvwprintw(mission_win, 4, 3, "3. Passthrough Gate more than One times ( %c )", stage1_mission3);
+    wattroff(mission_win, A_DIM);
+
     wrefresh(mission_win);
 }
 
